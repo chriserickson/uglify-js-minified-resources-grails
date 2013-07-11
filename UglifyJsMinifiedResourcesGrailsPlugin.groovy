bc6471c83f6542366b283926eac77a3e60e48dff
Chris Erickson <cperickson@gmail.com>
2013-07-11 07:56:52 -0600

    Issue #2: Updated plugin page links

--- UglifyJsMinifiedResourcesGrailsPlugin.groovy
+++ UglifyJsMinifiedResourcesGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class UglifyJsMinifiedResourcesGrailsPlugin {
     def loadAfter = ['resources']
-    def version = "0.1"
+    def version = "0.1.1"
     def grailsVersion = "2.0 > *"
     def title = "Uglify JS Minified Resources Plugin"
     def author = "Chris Erickson"
@@ -10,6 +10,6 @@ class UglifyJsMinifiedResourcesGrailsPlugin {
 
     def watchedResources = [ "file:./web-app/js/**/*.js", "file:./web-app/coffee/**/*.coffee", "file:./web-app/cs/**/*.coffee" ]
     def documentation = "https://github.com/chriserickson/uglify-js-minified-resources-grails"
-    def issueManagement = [ system: "GitHub", url: "https://github.com/chriserickson/uglify-js-minified-resource-grails/issues" ]
-    def scm = [url: 'https://github.com/chriserickson/uglify-js-minified-resource-grails']
+    def issueManagement = [ system: "GitHub", url: "https://github.com/chriserickson/uglify-js-minified-resources-grails/issues" ]
+    def scm = [url: 'https://github.com/chriserickson/uglify-js-minified-resources-grails']
 }
